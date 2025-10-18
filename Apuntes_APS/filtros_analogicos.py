0358c51cd62f73611d0b8431dd1b829a6318edab
Tomás Altimare Bercovich <tomasalti@gmail.com>
2025-10-18 14:02:50 -0300

    TS4 Corregido

---
+++ Apuntes_APS/filtros_analogicos.py
@@ -0,0 +1,106 @@
+"""
+Created on Wed Aug 20 2025
+
+@author: Tomás Altimare Bercovich
+
+Descripción:
+------------
+
+"""
+
+#%% Importo los módulos y bibliotecas que voy a utilizar
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import fft, fftshift
+from scipy import signal as sig 
+import scipy.io as sio
+import scipy.signal.windows as window
+from scipy.signal import periodogram
+from scipy.io.wavfile import write
+
+#%%########################
+## Definiciones Globales ##
+###########################
+
+fs = 1000 # Frecuencia de sampleo
+N = 1000 # Cantidad de muestras
+ts = 1/fs # Tiempo entre muestras
+T_simulacion = N/fs # Tiempo total de simulación
+n = np.arange(N) # Las N muestras equiespaciadas
+
+#%%#########################
+## Funciones del Programa ##
+############################
+
+
+#%%###################
+## Filtro Analógico ##
+######################
+# %% -- Parámetros de Plantilla de Diseño del filtro --
+
+wp = 1 # frecuencia de corte o de paso (rad/s)
+ws = 5 # frecuencia de stop (rad/s)
+
+alpha_p = 1 # atenuación máxima a la wp, alpha_max, perdidas de banda de paso
+alpha_s = 40 # atenuación minima a la ws, alpha_min, mínima atenuación requerida en banda de paso
+
+# Aproximadores de módulo:
+f_aprox = ['butter', 'cheby1', 'cheby2', 'ellip']
+
+# %% -- Diseño del filtro (en sampleos) --
+for i in range(0, 4):
+    b, a = sig.iirdesign(wp = wp, ws = ws, gpass = alpha_p, gstop = alpha_s, 
+                         analog = True, ftype = f_aprox[i], output = 'ba')
+    # Respuesta en frecuencia del filtro (calculada) 
+    w, h = sig.freqs(b, a)
+    phase = np.unwrap(np.angle(h)) # fase del grupo
+    gd = -np.diff(phase) / np.diff(w) # retardo
+
+    # %%  -- Calculo polos y ceros --
+    z, p, k = sig.tf2zpk(b, a) # Zpk = [ [z0,z1,...,zn], [p0,p1,...,pn], k]
+    
+    # %% Grafico
+    # Magnitud
+    plt.figure(1)
+    plt.semilogx(w, 20*np.log10(abs(h)), label = f"{f_aprox[i]}")
+    plt.title('Figura 1: Respuesta en Magnitud')
+    plt.xlabel('Pulsación angular [r/s]')
+    plt.ylabel('|H(jω)| [dB]')
+    plt.xlim(0.1, 10)
+    plt.grid(True, which='both', ls=':')
+    plt.legend()
+    
+    # Fase
+    plt.figure(2)
+    plt.semilogx(w, np.degrees(phase), label = f"{f_aprox[i]}")
+    plt.title('Figura 2: Fase')
+    plt.xlabel('Pulsación angular [r/s]')
+    plt.ylabel('Fase [°]')
+    plt.xlim(0.1, 10)
+    plt.grid(True, which='both', ls=':')
+    plt.legend()
+    
+    # Retardo de grupo
+    plt.figure(3)
+    plt.semilogx(w[:-1], gd, label = f"{f_aprox[i]}")
+    plt.title('Figura 3: Retardo de Grupo')
+    plt.xlabel('Pulsación angular [r/s]')
+    plt.ylabel('τg [s]')
+    plt.xlim(0.1, 10)
+    plt.grid(True, which='both', ls=':')
+    plt.legend()
+    
+    # Diagrama de polos y ceros
+    plt.figure(4)
+    plt.plot(np.real(p), np.imag(p), 'x', markersize=10, label= f'Polos de {f_aprox[i]}')
+    if len(z) > 0:
+        plt.plot(np.real(z), np.imag(z), 'o', markersize=10, fillstyle='none', label=f'Ceros de {f_aprox[i]}')
+    plt.axhline(0, color='k', lw=0.5)
+    plt.axvline(0, color='k', lw=0.5)
+    plt.title('Figura 4: Diagrama de Polos y Ceros (plano s)')
+    plt.xlabel('σ [rad/s]')
+    plt.ylabel('jω [rad/s]')
+    plt.legend()
+    plt.grid(True)
+
+sos = sig.tf2sos(b, a, analog = True)
